20fc924a54c35aae596c0c2892d57c636799629a
Dev Patel <devarshi.ap@gmail.com>
2022-12-29 11:24:03 -0500

    get random quote endpoint

---
+++ pages/api/random.ts
@@ -0,0 +1,17 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import path from "path";
+import { promises as fs } from "fs"
+
+type Data = {
+    quote: string;
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    const jsonDir = path.join(process.cwd(), 'json');
+    const fileContents = await fs.readFile(jsonDir + '/quotes.json', 'utf8')
+    var quotes_obj: { quotes: string[] } = JSON.parse(fileContents.toString());
+    var random_quote = quotes_obj.quotes[Math.floor(Math.random()*quotes_obj.quotes.length)];
+
+    res.status(200).json({ quote: random_quote });
+}
