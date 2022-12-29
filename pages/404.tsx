8a3c1f319122b2362ca5a24845bf3f62e6a0a0b8
Dev Patel <devarshi.ap@gmail.com>
2022-12-29 00:29:37 -0500

    completed static page

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+export default function Custom404() {
+    return (
+        <div className="flex flex-col w-[85%] mx-auto mt-20">
+
+            <div className="flex flex-col w-full items-center place-content-center h-[40vh] font-mono space-y-5" >
+                <span className="text-6xl animate-pulse">🏴‍☠️</span>
+                <h1 className="text-2xl">Ahoy matey! This here be a 404 page.</h1>
+            </div>
+        </div>
+    )
+}
