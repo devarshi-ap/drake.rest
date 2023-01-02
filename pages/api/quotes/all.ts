ea5fea73806e3cbb91f5e8e2d05c285b005e396e
Dev Patel <devarshi.ap@gmail.com>
2023-01-02 14:21:37 -0500

    refactor response JSON

--- pages/api/quotes/all.ts
+++ pages/api/quotes/all.ts
@@ -4,13 +4,14 @@ import path from "path";
 import { promises as fs } from "fs"
 
 type Data = {
-    quote: string[];
+    quotes: string[]
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
     const jsonDir = path.join(process.cwd(), 'json');
     const fileContents = await fs.readFile(jsonDir + '/quotes.json', 'utf8')
-    var quotes_obj: string[] = JSON.parse(fileContents.toString());
+    var quotes_obj: {quotes: string[]} = JSON.parse(fileContents.toString());
+    var quotes = quotes_obj.quotes;
 
-    res.status(200).json({ quote: quotes_obj });
+    res.status(200).json({ quotes });
 }
